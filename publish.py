5d141d62a19721f7e955b4d6568ed6befa289238
Rushi0 <51735506+Rushi0@users.noreply.github.com>
2020-06-06 16:00:25 +0530

    python code to control motor

---
+++ publish.py
@@ -0,0 +1,47 @@
+import time
+import sys
+import ibmiotf
+import ibmiotf.application
+import ibmiotf.device
+
+organization='a5yxee'
+deviceType='iotdevice'
+deviceId='motor'
+authMethod='token'
+authToken='rushi123'
+
+def myCommandCallback(cmd):
+    print('Command received: %s' % cmd.data)
+    if cmd.data['command']=='motoron':
+        print('MOTOR ON')
+    elif cmd.data['command']=='motoroff':
+        print('MOTOR OFF')
+    data={'Command' : cmd.data['command']}
+    success=deviceCli.publishEvent('event', 'json' ,data, qos=0, on_publish=myOnPublishCallback)
+    if not success:
+        print("Not Connected to IoTF")
+    myCommandCallback.has_been_called = True
+try:
+    deviceOptions= {"org": organization, 'type': deviceType, 'id': deviceId, 'auth-method': authMethod, 'auth-token': authToken}
+    deviceCli=ibmiotf.device.Client(deviceOptions)
+except Exception as e:
+    print("Caught exception connecting device: %s" % str(e))
+    sys.exit()
+deviceCli.connect()
+
+while True:
+    myCommandCallback.has_been_called= False
+    status='Sensor is ON'
+    data={'Status': status}
+    connected=False
+    def myOnPublishCallback():
+        print(data,"to IBM Watson")
+        connected=True
+    if not connected:
+        success=deviceCli.publishEvent('event', 'json' ,data, qos=0, on_publish=myOnPublishCallback)
+    if not success:
+        print("Not connected to IoTF")
+    time.sleep(1)
+    deviceCli.commandCallback = myCommandCallback
+    if myCommandCallback.has_been_called:
+        print("CALL MADE")
